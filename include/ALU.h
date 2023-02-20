48241082613d269144ee0709f257cc5c6a124a57
Ilias Chanis <iliashanis@gmail.com>
2023-02-20 11:13:54 +0200

    Create basic arithmetic and logical ALU functions
    Specifically, signed arithmetic addition, multiplication and subtraction
    and bitwise and, or, xor.

--- include/ALU.h
+++ include/ALU.h
@@ -3,7 +3,15 @@
 
 class ALU {
 public:
+    // arithmetic operations
     ac_int<32,true> add(ac_int<32,true>, ac_int<32,true>);
+    ac_int<32,true> sub(ac_int<32,true>, ac_int<32,true>);
+    ac_int<32,true> mul(ac_int<32,true>, ac_int<32,true>);
+
+    // bitwise logical operations
+    ac_int<32,true> bwand(ac_int<32,true>, ac_int<32,true>);
+    ac_int<32,true> bwor(ac_int<32,true>, ac_int<32,true>);
+    ac_int<32,true> bwxor(ac_int<32,true>, ac_int<32,true>);
 };
 
 #endif
--- src/ALU.cpp
+++ src/ALU.cpp
@@ -1,6 +1,28 @@
 #include "ac_int.h"
 #include "ALU.h"
 
-ac_int<32,true> ALU::add(ac_int<32,true> operand_1, ac_int<32,true> operand_2) {
-    return operand_1 + operand_2;
+// Arithmetic Operations
+ac_int<32,true> ALU::add(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 + op2;
+}
+
+ac_int<32,true> ALU::sub(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 - op2;
+}
+
+ac_int<32,true> ALU::mul(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 * op2;
+}
+
+// Bitwise Logical Operations
+ac_int<32,true> ALU::bwand(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 & op2;
+}
+
+ac_int<32,true> ALU::bwor(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 | op2;
+}
+
+ac_int<32,true> ALU::bwxor(ac_int<32,true> op1, ac_int<32,true> op2) {
+    return op1 ^ op2;
 }
